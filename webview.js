9906eaf5b7ca582d8f95e7118cff092e03dbb91e
Kai Brune <mail@brune.io>
2021-01-01 19:21:13 +0100

    fixed lang

--- webview.js
+++ webview.js
@@ -18,12 +18,12 @@ module.exports = (Ferdi) => {
   document.addEventListener("DOMContentLoaded", function(){
     var breadcrump = document.getElementById("vap-brdcrmb");
     if(breadcrump!=null){
-      breadcrump.innerHTML ='<a class="back-button" href="https://www.ebay-kleinanzeigen.de/m-nachrichten.html"><span class="Button--Body"><span>Zurück zum Messenger</span></span></a>' + breadcrump.innerHTML;
+      breadcrump.innerHTML ='<a class="back-button" href="/m-nachrichten.html"><span class="Button--Body"><span>Return to messenger</span></span></a>' + breadcrump.innerHTML;
     }
 
     var error_msg = document.getElementById("my-manageads-content");
     if(error_msg!=null){
-    error_msg.innerHTML ='<a class="back-button" href="https://www.ebay-kleinanzeigen.de/m-nachrichten.html"><span class="Button--Body"><span>Zurück zum Messenger</span></span></a>' + error_msg.innerHTML;
+    error_msg.innerHTML ='<a class="back-button" href="/m-nachrichten.html"><span class="Button--Body"><span>Return to messenger</span></span></a>' + error_msg.innerHTML;
     }
 });
 
